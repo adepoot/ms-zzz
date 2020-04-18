d125de3f5fe855d588e51a7234cb4f06092bbda3
Anton Depoot <anton.depoot@hotmail.com>
2020-04-18 17:37:22 +0200

    Integrate Flyway for db  migrations

--- src/main/resources/db/migration/V1__create_players.sql
+++ src/main/resources/db/migration/V1__create_players.sql
@@ -4,7 +4,7 @@ CREATE TABLE players (
     last_name   VARCHAR     NOT NULL,
     nickname    VARCHAR,
     email       VARCHAR,
-    birthday    TIMESTAMP WITH TIME ZONE,
+    birthday    DATE,
     created_at  TIMESTAMP WITH TIME ZONE NOT NULL   DEFAULT (now() at time zone 'utc'),
     deleted_at  TIMESTAMP WITH TIME ZONE
 )

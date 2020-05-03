a9cb76f32ee179a06ef1c3ad52c12d9867704729
Anton Depoot <anton.depoot@hotmail.com>
2020-05-03 20:54:50 +0200

    Add players to database

---
+++ src/main/resources/db/migration/V2__add_players.sql
@@ -0,0 +1,7 @@
+INSERT INTO players (first_name, last_name, nick_name, email, birthday)
+VALUES
+	('Alexandre', 'da Silva', 'Pato', 'apato@gmail.com', '1989-09-02'),
+	('Thierry', 'Henri', null, 'th@gmail.com', '1977-08-17'),
+	('Kevin', 'De Bruyne', null, 'debruyne@gmail.com', '1991-06-28'),
+	('Gianluigi', 'Buffon', 'Gigi', 'gigi@gmail.com', '1978-01-28'),
+	('Andrea', 'Pirlo', 'Il Maestro', 'pirlo@gmail.com', '1979-05-19');

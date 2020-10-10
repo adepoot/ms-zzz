63a68d4c188114c6e896239faea119d6ff3af7ff
Anton Depoot <anton.depoot@hotmail.com>
2020-10-10 10:14:17 +0200

    Add teams and players to database

---
+++ src/main/resources/db/migration/V7__data_add_teams.sql
@@ -0,0 +1,14 @@
+INSERT INTO teams (name)
+VALUES
+('De Zwarte Zeverzwijnen'),
+('Street United'),
+('ZVC SCAH'),
+('Boiler Boys'),
+('De Max FC'),
+('Excelsior FIT Group'),
+('Red-Side Academy'),
+('ZVC Runa'),
+('Liquor Pool FC'),
+('Celtic Glasvol'),
+('FC Janson Floor'),
+('ZVC Leie Shotters');
---
+++ src/main/resources/db/migration/V8__data_add_players.sql
@@ -0,0 +1,12 @@
+INSERT INTO players (first_name, last_name, number)
+VALUES
+('Mathijs', 'Delabie', 3),
+('Auryn', 'Bonte', 4),
+('Guillame', 'Deprez', 6),
+('Anton', 'Depoot', 7),
+('Waut', 'D''hondt', 9),
+('Rémi', 'Bossard', 10),
+('Thomas', 'Van Ranst', 11),
+('Laurens', 'Chantrie', 12),
+('Victor', 'Allaert', 98),
+('Tiebe', 'Van Boven', 99);

8f4d5bf16a0ad42e93fe9979c6fb07ad3aadf155
Anton Depoot <anton.depoot@gemone.com>
2021-11-07 15:34:04 +0100

    Correct numbers of players

--- src/main/resources/db/migration/V15__data_new_season.sql
+++ src/main/resources/db/migration/V15__data_new_season.sql
@@ -11,15 +11,7 @@ VALUES ('Dnweetjdathetmoeilijkwrdt'),
        ('De Zweetdieven'),
        ('Cluzona Kluisbergen');
 
-UPDATE players p
-SET number = 13
-WHERE p.first_name = 'Victor';
-
-UPDATE players p
-SET number = 5
-WHERE p.first_name = 'Tiebe';
-
 INSERT INTO players (first_name, last_name, number)
-VALUES ('Maxime', 'De Leyn', 74),
+VALUES ('Maxime', 'De Leyn', 95),
        ('Emiel', 'Deprez', 69);
 
--- src/main/resources/db/migration/V8__data_add_players.sql
+++ src/main/resources/db/migration/V8__data_add_players.sql
@@ -2,11 +2,11 @@ INSERT INTO players (first_name, last_name, number)
 VALUES
 ('Mathijs', 'Delabie', 3),
 ('Auryn', 'Bonte', 4),
+('Tiebe', 'Van Boven', 5),
 ('Guillaume', 'Deprez', 6),
 ('Anton', 'Depoot', 7),
 ('Waut', 'D''hondt', 9),
 ('Rémi', 'Bossard', 10),
 ('Thomas', 'Van Ranst', 11),
 ('Laurens', 'Chantrie', 12),
-('Victor', 'Allaert', 98),
-('Tiebe', 'Van Boven', 99);
+('Victor', 'Allaert', 13);

2ddc142e7fa0984bbdfdea91fea18ea372d12457
Anton Depoot <anton.depoot@gemone.com>
2022-02-15 08:37:05 +0100

    Add games 11/01/2022 and 24/01/2022

---
+++ src/main/resources/db/migration/V27__data_add_2022-01-11_game.sql
@@ -0,0 +1,37 @@
+INSERT INTO games (date, home_team_id, away_team_id, home_score, away_score)
+SELECT '2022-01-11', t1.id, t2.id, 4, 7
+FROM teams t1, teams t2
+WHERE
+    t1.name = 'De Zwarte Zeverzwijnen' AND
+    t2.name = 'Dnweetjdathetmoeilijkwrdt';
+
+INSERT INTO game_player (game_id, player_id)
+SELECT game.id, p.id FROM games game, players p
+WHERE
+    game.date = '2022-01-11' AND
+    p.first_name IN ('Rémi', 'Guillaume', 'Auryn', 'Anton', 'Maxime', 'Emiel', 'Behnur');
+
+INSERT INTO goals (game_id, scorer_id, assister_id)
+SELECT game.id, goal.id, assist.id from games game,players goal,players assist
+WHERE
+        game.date = '2022-01-11' AND
+        goal.first_name = 'Auryn' AND
+        assist.first_name = 'Anton';
+
+INSERT INTO goals (game_id, scorer_id)
+SELECT game.id, goal.id from games game, players goal
+WHERE
+        game.date = '2022-01-11' AND
+        goal.first_name = 'Rémi';
+
+INSERT INTO saves (game_id, player_id, count)
+SELECT g.id, p.id, 6 FROM games g, players p
+WHERE
+        g.date = '2022-01-11' AND
+        p.first_name = 'Rémi';
+
+INSERT INTO saves (game_id, player_id, count)
+SELECT g.id, p.id, 4 FROM games g, players p
+WHERE
+        g.date = '2022-01-11' AND
+        p.first_name = 'Behnur';
---
+++ src/main/resources/db/migration/V28__data_add_2022-01-24_game.sql
@@ -0,0 +1,58 @@
+INSERT INTO games (date, home_team_id, away_team_id, home_score, away_score)
+SELECT '2022-01-24', t1.id, t2.id, 3, 5
+FROM teams t1, teams t2
+WHERE
+    t1.name = 'Excelsior Qastan' AND
+    t2.name = 'De Zwarte Zeverzwijnen';
+
+INSERT INTO game_player (game_id, player_id)
+SELECT game.id, p.id FROM games game, players p
+WHERE
+    game.date = '2022-01-24' AND
+    p.first_name IN ('Rémi', 'Guillaume', 'Auryn', 'Anton', 'Maxime', 'Behnur');
+
+INSERT INTO goals (game_id, scorer_id, assister_id)
+SELECT game.id, goal.id, assist.id from games game,players goal,players assist
+WHERE
+        game.date = '2022-01-24' AND
+        goal.first_name = 'Anton' AND
+        assist.first_name = 'Guillaume';
+
+INSERT INTO goals (game_id, scorer_id, assister_id)
+SELECT game.id, goal.id, assist.id from games game,players goal,players assist
+WHERE
+        game.date = '2022-01-24' AND
+        goal.first_name = 'Behnur' AND
+        assist.first_name = 'Anton';
+
+INSERT INTO goals (game_id, scorer_id, assister_id)
+SELECT game.id, goal.id, assist.id from games game,players goal,players assist
+WHERE
+        game.date = '2022-01-24' AND
+        goal.first_name = 'Behnur' AND
+        assist.first_name = 'Maxime';
+
+INSERT INTO goals (game_id, scorer_id, assister_id)
+SELECT game.id, goal.id, assist.id from games game,players goal,players assist
+WHERE
+        game.date = '2022-01-24' AND
+        goal.first_name = 'Behnur' AND
+        assist.first_name = 'Anton';
+
+INSERT INTO goals (game_id, scorer_id)
+SELECT game.id, goal.id from games game, players goal
+WHERE
+        game.date = '2022-01-24' AND
+        goal.first_name = 'Anton';
+
+INSERT INTO saves (game_id, player_id, count)
+SELECT g.id, p.id, 6 FROM games g, players p
+WHERE
+        g.date = '2022-01-24' AND
+        p.first_name = 'Rémi';
+
+INSERT INTO saves (game_id, player_id, count)
+SELECT g.id, p.id, 6 FROM games g, players p
+WHERE
+        g.date = '2022-01-24' AND
+        p.first_name = 'Behnur';

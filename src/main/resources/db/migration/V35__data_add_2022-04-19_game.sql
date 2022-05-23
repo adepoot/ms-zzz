1db81a9f1620acb1e6f0d8a05e2b2c0f26b1e919
Anton Depoot <anton.depoot@gemone.com>
2022-05-23 18:49:11 +0200

    Add game 19/04/2022

---
+++ src/main/resources/db/migration/V35__data_add_2022-04-19_game.sql
@@ -0,0 +1,44 @@
+INSERT INTO games (date, home_team_id, away_team_id, home_score, away_score)
+SELECT '2022-04-19', t1.id, t2.id, 6, 8
+FROM teams t1, teams t2
+WHERE
+    t1.name = 'De Zwarte Zeverzwijnen' AND
+    t2.name = 'Excelsior Qastan';
+
+INSERT INTO game_player (game_id, player_id)
+SELECT game.id, p.id FROM games game, players p
+WHERE
+    game.date = '2022-04-19' AND
+    p.first_name IN ('Victor', 'Rémi', 'Auryn', 'Mathijs', 'Maxime', 'Emiel', 'Guillaume', 'Anton');
+
+INSERT INTO goals (game_id, scorer_id, assister_id)
+SELECT game.id, goal.id, assist.id from games game,players goal,players assist
+WHERE
+        game.date = '2022-04-19' AND
+        goal.first_name = 'Emiel' AND
+        assist.first_name = 'Rémi';
+
+INSERT INTO goals (game_id, scorer_id, assister_id)
+SELECT game.id, goal.id, assist.id from games game,players goal,players assist
+WHERE
+        game.date = '2022-04-19' AND
+        goal.first_name = 'Rémi' AND
+        assist.first_name = 'Emiel';
+
+INSERT INTO goals (game_id, scorer_id)
+SELECT game.id, goal.id from games game, players goal
+WHERE
+        game.date = '2022-04-19' AND
+        goal.first_name = 'Emiel';
+
+INSERT INTO goals (game_id, scorer_id)
+SELECT game.id, goal.id from games game, players goal
+WHERE
+        game.date = '2022-04-19' AND
+        goal.first_name = 'Emiel';
+
+INSERT INTO saves (game_id, player_id, count)
+SELECT g.id, p.id, 20 FROM games g, players p
+WHERE
+        g.date = '2022-04-19' AND
+        p.first_name = 'Victor';

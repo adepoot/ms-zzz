21b2087ee94e3fd2475629f1f9783b700e5940c4
Anton Depoot <anton.depoot@gemone.com>
2022-05-23 19:02:11 +0200

    Add game 18/05/2022

---
+++ src/main/resources/db/migration/V37__data_add_2022-05-18_game.sql
@@ -0,0 +1,71 @@
+INSERT INTO games (date, home_team_id, away_team_id, home_score, away_score)
+SELECT '2022-05-18', t1.id, t2.id, 14, 9
+FROM teams t1, teams t2
+WHERE
+    t1.name = 'Runa' AND
+    t2.name = 'De Zwarte Zeverzwijnen';
+
+INSERT INTO game_player (game_id, player_id)
+SELECT game.id, p.id FROM games game, players p
+WHERE
+    game.date = '2022-05-18' AND
+    p.first_name IN ('Auryn', 'Mathijs', 'Tiebe', 'Victor', 'Guillaume', 'Anton');
+
+INSERT INTO goals (game_id, scorer_id, assister_id)
+SELECT game.id, goal.id, assist.id from games game,players goal,players assist
+WHERE
+        game.date = '2022-05-18' AND
+        goal.first_name = 'Auryn' AND
+        assist.first_name = 'Tiebe';
+
+INSERT INTO goals (game_id, scorer_id, assister_id)
+SELECT game.id, goal.id, assist.id from games game,players goal,players assist
+WHERE
+        game.date = '2022-05-18' AND
+        goal.first_name = 'Tiebe' AND
+        assist.first_name = 'Mathijs';
+
+INSERT INTO goals (game_id, scorer_id, assister_id)
+SELECT game.id, goal.id, assist.id from games game,players goal,players assist
+WHERE
+        game.date = '2022-05-18' AND
+        goal.first_name = 'Tiebe' AND
+        assist.first_name = 'Mathijs';
+
+INSERT INTO goals (game_id, scorer_id, assister_id)
+SELECT game.id, goal.id, assist.id from games game,players goal,players assist
+WHERE
+        game.date = '2022-05-18' AND
+        goal.first_name = 'Tiebe' AND
+        assist.first_name = 'Anton';
+
+INSERT INTO goals (game_id, scorer_id, assister_id)
+SELECT game.id, goal.id, assist.id from games game,players goal,players assist
+WHERE
+        game.date = '2022-05-18' AND
+        goal.first_name = 'Anton' AND
+        assist.first_name = 'Guillaume';
+
+INSERT INTO goals (game_id, scorer_id)
+SELECT game.id, goal.id from games game, players goal
+WHERE
+        game.date = '2022-05-18' AND
+        goal.first_name = 'Mathijs';
+
+INSERT INTO goals (game_id, scorer_id)
+SELECT game.id, goal.id from games game, players goal
+WHERE
+        game.date = '2022-05-18' AND
+        goal.first_name = 'Anton';
+
+INSERT INTO goals (game_id, scorer_id)
+SELECT game.id, goal.id from games game, players goal
+WHERE
+        game.date = '2022-05-18' AND
+        goal.first_name = 'Anton';
+
+INSERT INTO saves (game_id, player_id, count)
+SELECT g.id, p.id, 28 FROM games g, players p
+WHERE
+        g.date = '2022-05-18' AND
+        p.first_name = 'Victor';

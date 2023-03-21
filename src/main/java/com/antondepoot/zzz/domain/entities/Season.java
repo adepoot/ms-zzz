f8b2e2fc53f6b53f22c637e9c919a342fc5879ca
Anton Depoot <anton.depoot@gemone.com>
2023-03-21 19:13:30 +0100

    Update to season 2022-2023

--- src/main/java/com/antondepoot/zzz/domain/entities/Season.java
+++ src/main/java/com/antondepoot/zzz/domain/entities/Season.java
@@ -9,7 +9,8 @@ import java.time.Instant;
 @AllArgsConstructor
 public enum Season {
     SEASON_2020_2021(Instant.parse("2020-09-01T00:00:00.000Z"), Instant.parse("2021-08-31T00:00:00.000Z")),
-    SEASON_2021_2022(Instant.parse("2021-09-01T00:00:00.000Z"), Instant.parse("2022-08-31T00:00:00.000Z"));
+    SEASON_2021_2022(Instant.parse("2021-09-01T00:00:00.000Z"), Instant.parse("2022-08-31T00:00:00.000Z")),
+    SEASON_2022_2023(Instant.parse("2022-09-01T00:00:00.000Z"), Instant.parse("2023-08-31T00:00:00.000Z"));
 
     final Instant start;
     final Instant end;
